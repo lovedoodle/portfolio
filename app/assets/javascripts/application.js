080461fe3f57061766affc8b439cbf3de8f29526
Jing Wait <jingzhaowait@gmail.com>
2016-03-16 23:45:24 -0700

    add mobile drop down menu butten

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+ 
+$(document).ready(function() {
+  $(".phoneMenu").click(
+    function(){
+     $(this).find("ul").toggle();
+      
+    }
+  );
+});
